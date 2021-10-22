251e121f276c53ec042672d6d5d665b8b7b21ca7
proger0000 <proger0000@gmail.com>
2021-10-22 10:50:23 +0300

    начал управление

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    public void SetDirection(float direction);
+    [SerializeField] private float _speed;
+    private float _direction;
+
+    public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+        
+    }
 }
--- Assets/_Scripts/HeroInput.cs
+++ Assets/_Scripts/HeroInput.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class HeroInput : MonoBehaviour
 {
-    
+    private Hero _hero;
+
+    private void Awake()
+    {
+        _hero = GetComponent<Hero>();
+    }
     public void Update ()
     {
         
         if (Input.GetKey(KeyCode.A)) 
         {
+            _hero.SetDirection(-1);
 
         }else if(Input.GetKey(KeyCode.D))
-
         {
-            
+            _hero.SetDirection(1);
+        }
+        else
+        {
+            _hero.SetDirection(0);
         }
 
     }
