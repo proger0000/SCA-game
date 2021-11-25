66cc5d8a1c7707d1cbd77e60ce5187ed5df83862
proger0000 <proger0000@gmail.com>
2021-11-25 21:38:49 +0200

    cheats input
    ##reset
    сортировка файлов

---
+++ Assets/_Scripts/CheatController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class CheatController : MonoBehaviour
+{
+    [SerializeField] private float _inputTimeToLive;
+    [SerializeField] private CheatItem[] _cheats;
+
+    private string _currentInput;
+    private float _inputTime;
+
+    private void Awake()
+    {
+        Keyboard.current.onTextInput += OnTextInput;
+    }
+
+    private void OnDestroy()
+    {
+        Keyboard.current.onTextInput -= OnTextInput;
+    }
+
+    private void OnTextInput(char inputChar)
+    {
+        _currentInput += inputChar;
+        _inputTime = _inputTimeToLive;
+        FindAnyCheats();
+    }
+
+    private void FindAnyCheats()
+    {
+        foreach (var cheatItem in _cheats)
+        {
+            if (_currentInput.Contains(cheatItem.Name))
+            {
+                cheatItem.Action.Invoke();
+                _currentInput = string.Empty;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_inputTime < 0)
+        {
+            _currentInput = string.Empty;
+        }
+        else
+        {
+            _inputTime -= Time.deltaTime;
+        }
+    }
+    
+    [Serializable]
+    public class CheatItem
+    {
+        public string Name;
+        public UnityEvent Action;
+    }
+}
+
